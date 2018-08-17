043632c9968271b3606f060fce4e6fa1ed1b35a8
Israel Vinicius <israelvinicius14@hotmail.com>
2018-08-17 08:19:00 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LotteryContainer from './Components/LotteryContainer.js';
+
+class App extends React.Component {
+  render() {
+    return <LotteryContainer/>
+  }
+}
+
+export default App;
---
+++ src/Components/LotteryContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class LotteryContainer extends React.Component{
+    render(){
+        return <p>Ola React</p>
+    }
+}
+
+export default LotteryContainer;
